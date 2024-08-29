ac3ed60d9faa9e77ba6310b8e84436143b749cde
Nathan Henrie <nate@n8henrie.com>
2024-08-29 17:08:09 -0600

    Show `warn` by default

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{self, EnvFilter};
 
 use update_pypi_deps::{run, Result};
@@ -5,7 +6,9 @@ use update_pypi_deps::{run, Result};
 fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
-            EnvFilter::from_default_env()
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::WARN.into())
+                .from_env_lossy()
                 .add_directive("hyper=warn".parse()?)
                 .add_directive("reqwest=warn".parse()?),
         )
